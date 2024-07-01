34356950d5a8c8b0677d4aff5316bebf4914c8d1
Zhuk Artsiom <temagrace@hotmail.com>
2024-07-01 14:56:24 +0300

    создали метод получение исходного массива и проверили его вывод

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Ведите количество строк: " );
+int line = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите строки: " );
+string[] origArray = new string[line + 1];
+
+for (int i = 0; i < line; i++)
+{
+    origArray[i] = Console.ReadLine();
+}
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]+ " ");
+    }
+}
+PrintArray(origArray);
