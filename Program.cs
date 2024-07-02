824fdcf7f95e675d78cb6ff932b2a569ec104e89
Zhuk Artsiom <temagrace@hotmail.com>
2024-07-02 13:21:39 +0300

    исправили последние ошибки и вывод ответа

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 ﻿
+
 // содание исходного массива строк
+
+
 System.Console.WriteLine("Ведите количество строк: " );
 int line = Convert.ToInt32(Console.ReadLine());
 
 System.Console.WriteLine("Введите строки: " );
-string[] origArray = new string[line + 1];
+string[] origArray = new string[line];
 
 for (int i = 0; i < line; i++)
 {
     origArray[i] = Console.ReadLine();
 }
+
+// вывод массивов
 void PrintArray(string[] array)
 {
     for(int i = 0; i < array.Length; i++)
@@ -17,35 +22,48 @@ void PrintArray(string[] array)
         System.Console.Write(array[i]+ " ");
     }
 }
-PrintArray(origArray);
 
 
-// string[] newArray(string[] array)
-// {
-//     int j = 0;
-//     string[] newArray = new string[j];
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i].Length <= 3)
-//         {
-//             newArray[j] = array[i];
-//             j++;
-//         }
 
-//     }
-//     return newArray;
-// }
-// string[] newOrigArray = newArray(origArray);
-// PrintArray(newOrigArray);
- int indexNewArray( string[] array)
+// содание нового массива
+string[] newArray(string[] array, int length)
+{
+    int newI = 0;
+    string[] newArray = new string[length];
+    for (int i = 0; i < array.Length; i++)
+    {    
+        if (array[i].Length <= 3)
+        {
+            newArray[newI] = array[i];
+            newI++;
+        }
+
+    }
+    return newArray;
+}
+
+//определене длинны нового массива
+int indexNewArray(string[] array)
  {
+    
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            count++;
+           count += 1;
         }
-    }
+    }   
     return count;
- }
+    }
+
+ // вывод результата
+
+PrintArray(origArray);
+int newLine = indexNewArray(origArray);
+string[] newOrigArray = newArray(origArray, newLine);
+System.Console.Write(" => ");
+PrintArray(newOrigArray);
+
+
+ 
